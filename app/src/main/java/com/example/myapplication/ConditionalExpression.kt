8dea9ab450ac81ea9368b477f817485757cae300
wawanpi <muhaguskurniawan@gmail.com>
2024-10-12 21:58:48 +0700

    membuat  perulangan Try-Catch

--- app/src/main/java/com/example/myapplication/ConditionalExpression.kt
+++ app/src/main/java/com/example/myapplication/ConditionalExpression.kt
@@ -25,4 +25,41 @@ fun ConditionalStatement () {
         7 -> println("Saturday")
         else -> println("Invalid Day")
     }
-}
+    // Try-Catch
+    val value = "10"
+    try {
+        val result = value. toInt ()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
+    fun Perulangan() {
+        println()
+        println(" === Perulangan === ")
+
+        for (i in 1..5) {
+            println("Perulangan ke-$i")
+        }
+
+        val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+        for (car in cars) {
+            println("Car type: $car")
+        }
+        var x = 1
+        while (x <= 5) {
+            println("Perulangan ke-$x")
+            x++
+        }
+
+        var y = 1
+        do {
+            println("Perulangan ke-$y")
+            y++
+        } while (y <= 5)
+    }
+
+            fun main() {
+                ConditionalStatement ()
+                Perulangan ()
+            }
+    }
