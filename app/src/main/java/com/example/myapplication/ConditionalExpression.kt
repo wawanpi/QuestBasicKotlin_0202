fbea57f57eca08dc67fca98e59dc6c7d3ac7cbad
wawanpi <muhaguskurniawan@gmail.com>
2024-10-12 21:49:11 +0700

    membuat percabangan when

--- app/src/main/java/com/example/myapplication/ConditionalExpression.kt
+++ app/src/main/java/com/example/myapplication/ConditionalExpression.kt
@@ -13,4 +13,16 @@ fun ConditionalStatement () {
     } else {
         println("Negative Number")
     }
+    // When
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
 }
