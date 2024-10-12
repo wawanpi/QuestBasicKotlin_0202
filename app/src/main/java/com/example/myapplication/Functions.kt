3017f56f780f1d951499a0d521cca0b560f0fcea
wawanpi <muhaguskurniawan@gmail.com>
2024-10-12 22:18:12 +0700

    Membuat Functions

---
+++ app/src/main/java/com/example/myapplication/Functions.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+fun withoutParameter() {
+    println(" == withoutParameter == ")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println(" == withParameter == ")
+    println("Hello, $name!")
+}
+//named argument
+//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println(" == withNamedArgument == ")
+    println("Hello, $name! You are $age years old.")
+}
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = " Indra", age: Int) {
+    println()
+    println(" == withDefaultParameter == ")
+    println("Hello, $name! You are $age years old.")
+}
+fun withReturn(panjang: Int, lebar: Int): Int {
+    return println(panjang * lebar)
+}
+fun main() {
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25)
+}
